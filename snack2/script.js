54f4f93f812da589460ba13e68aaf6b1e6ae09be
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-03-29 17:29:40 +0100

    [UPD] Ex2 Added array object

---
+++ snack2/script.js
@@ -0,0 +1,42 @@
+//Esercizio 2
+//Creazione array object di squadre di calcio. Requisiti di proprietà: nome, punti, falli
+//Inolte, "name" sarà l'unica ad essere compilata, il resto settato a 0
+//Infine, ciò che è settato a 0, dovrà essere sostituito da numeri generati random
+
+const teams= [
+    {
+        name: "Juventus",
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: "Inter",
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: "Milan",
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: "Napoli",
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: "AS Roma",
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: "Parma",
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: "Udinese",
+        points: 0,
+        fouls: 0,
+    },
+]
